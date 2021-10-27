6a17194edca82021840eedb1622ee28abd5648ea
mmajde <marek.majde@gmail.com>
2021-10-27 17:01:41 +0200

    Create ruby-test.rb

---
+++ ruby-test.rb
@@ -0,0 +1,22 @@
+some text - ruby
+some text - #-------------------------------------------------------------------------
+some text - # Copyright (c) Microsoft. All rights reserved.
+some text - #--------------------------------------------------------------------------
+some text - 
+some text - module Azure
+  module Blob
+    class Blob
+
+      def initialize
+        @properties = {}
+        @metadata = {}
+        yield self if block_given?
+      end
+
+      attr_accessor :name
+      attr_accessor :snapshot
+      attr_accessor :properties
+      attr_accessor :metadata
+    end
+  end
+end
